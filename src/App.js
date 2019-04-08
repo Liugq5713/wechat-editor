ce95d1856fe69e3c157007669c24c318df2974e1
liugq <liugq5713@gmail.com>
2019-04-08 17:28:34 +0800

    pwa 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react'
 import Content from './Content'
 class App extends Component {
+  componentDidMount(){
+    window.addEventListener("beforeinstallprompt", function(e) { 
+      // log the platforms provided as options in an install prompt 
+      console.log(e.platforms); // e.g., ["web", "android", "windows"] 
+      e.userChoice.then(function(outcome) { 
+        console.log(outcome); // either "accepted" or "dismissed"
+      }); 
+    });
+  }
   render() {
     return (
       <div className='App'>
