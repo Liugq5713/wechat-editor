efd30a45bf75996849f537c7319601c264e95a44
liugq <liugq5713@gmail.com>
2019-04-11 18:32:16 +0800

    pwa cache all

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,13 @@
-const workboxPlugin = require('workbox-webpack-plugin')
-console.log('workboxPlugin', workboxPlugin)
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
 module.exports = function override(config, env) {
-  //do stuff with the webpack config...
+  config.plugins = config.plugins.map(plugin => {
+    if(plugin.constructor.name === 'GenerateSW') {
+      return new WorkboxWebpackPlugin.InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js'
+      })
+    }
+    return plugin
+  })
   return config
 }
